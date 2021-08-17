ac8355152b10f5d1cb8ecb64470a21a370e7667c
Anand Balaji <76686299+anand-2@users.noreply.github.com>
2021-08-17 16:54:16 +0530

    First Upload - DB & UI Created

---
+++ db/create.sql
@@ -0,0 +1,80 @@
+create table public.user
+(
+    id serial primary key,
+    username  varchar(200) not null,
+    password varchar(20) not null, 
+    email   varchar(200) not null,
+    location varchar(200) ,
+    phone_num int ,
+    is_owner boolean,
+    created_date date default now() not null,
+    created_by bigint references public.user(id),
+    updated_date date ,
+    updated_by varchar(100)
+);
+
+create table public.restaurant
+(
+    id serial primary key,
+    name varchar(200) not null,
+    description varchar(200) not null, 
+    address   varchar(200) not null,
+    location varchar(200) not null,
+    image text not null,
+    rating int,
+    owner_id int not null,
+    created_date date default now() not null,
+    created_by bigint references public.user(id) not null,
+    updated_date date ,
+    updated_by varchar(100)
+);
+
+create table public.dish
+(
+    id serial primary key,
+    restaurant_id bigint references restaurant(id),
+    name varchar(200) not null,
+    description varchar(200) not null, 
+    image text not null,
+    veg boolean not null,
+    rating int,
+    price float,
+    owner_id int not null,
+    created_date date default now() not null,
+    created_by bigint references public.user(id) not null,
+    updated_date date ,
+    updated_by varchar(100)
+);
+
+create table public.payment_type
+(
+    id serial primary key,
+    name varchar(100) not null
+);
+
+create table public.status
+(
+    id serial primary key,
+    name varchar(100) not null
+);
+
+create table public.order
+(
+    id serial primary key,
+    payment_method_id bigint references payment_type(id),
+    status_id bigint references status(id),
+    created_date date default now() not null,
+    created_by bigint references public.user(id) not null,
+);
+
+create table public.tag
+(
+    id serial primary key,
+    name varchar(100) not null 
+);
+
+create table public.dish_tag
+(
+    tag_id bigint references tag(id),
+    dish_id bigint references dish(id)
+); 
---
+++ db/insert.sql
@@ -0,0 +1,20 @@
+--Status
+insert into public.status(name) values('Order Received');
+insert into public.status(name) values('Preparing Food');
+insert into public.status(name) values('On the way');
+insert into public.status(name) values('Delivered');
+insert into public.status(name) values('Order Delayed');
+insert into public.status(name) values('Refund Initiated');
+
+--Payment_Type
+insert into public.payment_type(name) values('Credit Card');
+insert into public.payment_type(name) values('Debit Card');
+insert into public.payment_type(name) values('Meal Coupon');
+insert into public.payment_type(name) values('Netbanking');
+insert into public.payment_type(name) values('UPI');
+insert into public.payment_type(name) values('PayTM');
+insert into public.payment_type(name) values('Cash on Delivery');
+
+
+
+
