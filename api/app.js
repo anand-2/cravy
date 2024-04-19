72ed1cac0ae566812b529484a40ac712808bc0ab
Anand Balaji <76686299+anand-2@users.noreply.github.com>
2024-04-19 11:11:38 +0530

    port changed

--- api/app.js
+++ api/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 require("dotenv").config()
 var app = module.exports = express();
-const port = 5000
+const port = 9000
 
 var index_route = require('./routes/index')
 var users_route = require("./routes/users")
